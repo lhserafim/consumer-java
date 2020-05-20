0b816026a7c137e90a0afd3b5d680d0d14dafefc
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-05-20 15:00:14 -0300

    Projeto atualizado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,41 @@
 package application;
 
+import entities.Product;
+import util.PriceUpdate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Consumer;
+
 public class Program {
     public static void main(String args[]) {
+        Locale.setDefault(Locale.US);
 
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        // Modo
+        // Implementação da interface
+        //list.forEach(new PriceUpdate());
+
+        // Modo
+        // Expressão lambda declarada
+        //Consumer<Product> cons = p -> {
+        //    p.setPrice(p.getPrice() * 1.1);
+        //list.forEach(cons);
+        //};
+
+        // Modo
+        // Expressão lambda inline
+        list.forEach(p -> p.setPrice(p.getPrice() * 1.1));
+
+
+        // Imprimindo a lista de outra forma.
+        // Utilizando um reference method
+        list.forEach(System.out::println);
     }
 }
